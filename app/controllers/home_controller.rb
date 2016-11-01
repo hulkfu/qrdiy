35616cba0281184cee6898ae858bbffedbe2199b
Fu Hao <fudhao@gmail.com>
2016-11-01 14:18:14 +0800

    Now can gen qr.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,26 @@
 class HomeController < ApplicationController
   def index
-    @code = Code.new
   end
 
   def text
+    if text_params.present?
+      if text_params.include? :qr_id
+        @code = Code.find_by_id text_params[:qr_id]
+      elsif text_params[:qr_content].present?
+        @code = Code.create(content: text_params[:qr_content])
+        redirect_to "/text/#{@code.id}"
+      end
+    end
   end
 
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_code
+      @code = Code.try(:find, params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def text_params
+      params.permit([:qr_id, :qr_content])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   post "text", to: "home#text"
-  get "text", to: "home#text"
+  get "text(/:qr_id)", to: "home#text"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#text"
 end
