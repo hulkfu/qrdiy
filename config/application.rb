9a1233e99cca8f3142f40f339c1b022c83f0f67f
Fu Hao <fudhao@gmail.com>
2016-12-12 14:21:42 +0800

    load all models in app/models/ sub directory

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module QrdiyCom
   class Application < Rails::Application
+    # load all models in app/models/ sub directory
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
