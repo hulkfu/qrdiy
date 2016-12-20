35a1366492f48a4567a845ffb4c296f3069953c5
Fu Hao <fudhao@gmail.com>
2016-12-20 18:43:35 +0800

    add notification for add.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@
 class Notification < ActiveRecord::Base
   DEFAULT_AVATAR = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPAAAADwCAMAAAAJixmgAAAAFVBMVEWkpKSnp6eqqqq3t7fS0tLV1dXZ2dmshcKEAAAAtklEQVR4Ae3XsRGAAAjAQFRk/5HtqaTz5H+DlInvAQAAAAAAAAAAAAAAAAAAAACymiveO6o7BQsWLFiwYMGCBS8PFixYsGDBggULFixYsGDBggULFixYsGDBggULFixYsGDBc4IFCxYsWLBgwYIFC14ZfOeAPRQ8IliwYMGCBQsWLFiwYMGCBQsWLFiwYMGCBQsWLFiwYMGCBQsWLFiwYMGCBQv+JQAAAAAAAAAAAAAAAAAAAOAB4KJfdHmj+kwAAAAASUVORK5CYII='
 
-  enum notify_type: [:follow, :like]
+  enum notify_type: [:follow, :like, :add]
 
   belongs_to :actor, class_name: :User
   belongs_to :user
--- app/models/status.rb
+++ app/models/status.rb
@@ -3,6 +3,7 @@
 # 东西，比如 Idea 被 user 在 project 里发布后，就会创建一个 status，然后能够显示在 user 和
 # project 的动态列表里。也可以是 user follow 了另一个 user。
 class Status < ApplicationRecord
+  include Statusable
   # actions 的 enum 顺序不能变，因为数据库是按这个记的，从 0 往后拍
   ACTION_TYPE_NAMES = {add: "发布", change: "更新", remove: "删除"}.freeze
   enum action_type: ACTION_TYPE_NAMES.keys
@@ -16,6 +17,13 @@ class Status < ApplicationRecord
 
   # 默认最新的在前
   default_scope { order(created_at: :desc) }
+
+  after_create do
+    if user != statusable.user
+      Notification.create(actor: user, user: statusable.user, notificationable: self,
+        notify_type: "add")
+    end
+  end
 
   # OPTIMIZE 需要翻译成多种语言时，可以返回 t(status.name) 的翻译后的结果
   def action_name
