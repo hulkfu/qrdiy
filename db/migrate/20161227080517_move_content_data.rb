f4186b95040f305a1fdfbd9ccf454705941e157b
Hunter Foo <fudhao@gmail.com>
2016-12-27 23:05:23 +0800

    move_content_data.

--- db/migrate/20161227080517_move_content_data.rb
+++ db/migrate/20161227080517_move_content_data.rb
@@ -2,7 +2,7 @@ class MoveContentData < ActiveRecord::Migration[5.0]
   def change
     %w(attachments ideas comments image_arrays).each do |t|
       t[0..-2].classify.constantize.all.each do |r|
-        if r && r.publication
+        if r && r.publication && r.publication.content && r.publication.content.size > 0
           r.content = r.publication.content
           r.save!
         end
