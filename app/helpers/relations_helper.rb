7d5d3522a01b7286fda5dcbdf09e6aa013c3221e
Fu Hao <fudhao@gmail.com>
2016-12-28 13:36:22 +0800

    relation css.

--- app/helpers/relations_helper.rb
+++ app/helpers/relations_helper.rb
@@ -7,6 +7,7 @@ module RelationsHelper
   #  - action_type 关系的类别
   #  - submit_name 显示的提交按钮的内容
   #  - show_count 是否显示发生关系的人数
+  #  - class      生成的 div 的附加 class
   #  - button_class 显示的按钮的 class
   def relation_for(relationable, opts={})
     return "" if relationable.blank?
@@ -17,7 +18,7 @@ module RelationsHelper
       c = relationable.send("who_#{opts[:action_type]}").count
       count = "(#{c})" if c > 0
     end
-    content_tag(:div, class: "relation #{opts[:action_type]}") do
+    content_tag(:div, class: "relation #{opts[:action_type]} #{opts[:class]}") do
       if current_user && relation = current_user.send("#{opts[:action_type]}_relation", relationable)
         # TODO 可定义的 html
         content_tag(:span, class: "remove") do
