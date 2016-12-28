877615c551e783374a0d3baa87cdfbac70e8a66f
Fu Hao <fudhao@gmail.com>
2016-12-28 11:14:09 +0800

    add button class for relation for.

--- app/helpers/relations_helper.rb
+++ app/helpers/relations_helper.rb
@@ -7,6 +7,7 @@ module RelationsHelper
   #  - name 关系的类别
   #  - submit_name 显示的提交按钮的内容
   #  - show_count 是否显示发生关系的人数
+  #  - button_class 显示的按钮的 class
   def relation_for(relationable, opts={})
     return "" if relationable.blank?
     return if relationable.is_a? Relation
@@ -20,13 +21,14 @@ module RelationsHelper
       if current_user && relation = current_user.send("#{opts[:action_type]}_relation", relationable)
         # TODO 可定义的 html
         content_tag(:span, class: "remove") do
-          button_to relation, method: :delete do
+          button_to relation, method: :delete, class: opts[:button_class] do
              "已#{opts[:submit_name]}#{count}"
           end
         end
       else
         content_tag(:span, class: "add") do
           render "relations/form", relationable: relationable,
+            button_class: opts[:button_class],
             action_type: opts[:action_type],
             submit_name: opts[:submit_name]<<count
         end
