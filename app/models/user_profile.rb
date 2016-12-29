397e17fe24e19d8acc805d59510a8f6fd35f09e2
Fu Hao <fudhao@gmail.com>
2016-12-29 13:01:15 +0800

    profile avatar update.

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -6,5 +6,7 @@ class UserProfile < ApplicationRecord
 
   validates :name, presence: true, length: 2..20, uniqueness: {case_sensitive: false}
   validates :domain, presence: true, length: 4..18, uniqueness: {case_sensitive: false}
-  # validates :avatar, presence: true
+
+  # 反正只是更新，就不需要验证 presence 了，空就会默认不变了
+  validates :avatar, file_size: { less_than: 10.megabytes.to_i }
 end
