400d9a4c8115463dadb78af1d7c266daf7df2d00
Hunter Foo <fudhao@gmail.com>
2017-01-03 22:22:30 +0800

    publication redirect.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,17 +1,22 @@
 class StatusesController < ApplicationController
   before_action :set_status, only: [:show, :destroy]
 
-  # TODO: 处理 status 的锚点跳转
+  # 处理 status 的锚点跳转
   def show
     statusable = @status.statusable
     case statusable
+    when Project
+      redirect_to statusable
     when Relation
       case statusable.relationable
       when Project, User
         redirect_to statusable.relationable
+      when Publication
+        redirect_to_publication statusable.relationable
       end
+    when Publication
+      redirect_to_publication statusable
     end
-    render_404
   end
 
   def index
@@ -19,7 +24,6 @@ class StatusesController < ApplicationController
   end
 
   def destroy
-    authorize @status
     @status.destroy
 
     redirect_to :back
@@ -29,5 +33,11 @@ class StatusesController < ApplicationController
 
   def set_status
     @status = Status.find(params[:id])
+    authorize @status
+  end
+
+  # TODO: 跳转到 publication 的锚点，分辨出 comment
+  def redirect_to_publication(publication)
+    redirect_to project_path(@status.project, anchor: "status-#{@status.id}")
   end
 end
