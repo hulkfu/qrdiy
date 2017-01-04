d232102d90e82a07a2f36e2b47de60228f5cfd1a
Fu Hao <fudhao@gmail.com>
2017-01-04 18:18:38 +0800

    update

--- app/models/publications/comment.rb
+++ app/models/publications/comment.rb
@@ -1,7 +1,11 @@
 ##
 # Comment 只是是对 status 的评论。而 status 包含了所有的状态。
 # 如此结果了对多个东西进行评论的问题。
+#
 # Comment 需要独立吗？
+# 目前看不需要，因为虽然在 Publication 里处理主要逻辑，但该分离的已经分离了。
+# 主要是可以按需求正常运行，没有必要独立。实在满足不了，需要独立的话。只需要
+# 迁移数据库，并修改相关。工作量也不大。
 #
 class Comment < ApplicationRecord
   include Publishable
