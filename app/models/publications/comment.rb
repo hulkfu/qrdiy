083c9a3e1d09b4b165e9cab68652e7d6b195526e
Fu Hao <fudhao@gmail.com>
2017-01-04 18:10:44 +0800

    show statusable.

--- app/models/publications/comment.rb
+++ app/models/publications/comment.rb
@@ -1,3 +1,8 @@
+##
+# Comment 只是是对 status 的评论。而 status 包含了所有的状态。
+# 如此结果了对多个东西进行评论的问题。
+# Comment 需要独立吗？
+#
 class Comment < ApplicationRecord
   include Publishable
 
@@ -6,4 +11,8 @@ class Comment < ApplicationRecord
   auto_strip_attributes :content,
                         squish: true, nullify: false
   validates :content, presence: true, length: 1..500
+
+  def commentable
+    status.statusable
+  end
 end
