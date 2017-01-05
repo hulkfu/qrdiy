c26153d91e06f2147acdd28cc4c6bf1e6f352c24
Fu Hao <fudhao@gmail.com>
2017-01-05 15:52:52 +0800

    update

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -14,6 +14,12 @@ class StatusesController < ApplicationController
       # 1. 下面跳到 status 处。
       # 2. 再次来到 show 里，得到 status 的 statusable 后再跳，比如是个 publication。
       # 有些递归的意思，停止条件是：statusable 不再是 status。
+      #  举个例子：我 like 了一个 发布图片的 status，因此会产生一个 like status，同时这个
+      #     like status 会生成一个 notification 通知图片的发布者，而发布者能够通过点击
+      #     notification 里的 link 跳到具体的图片 status。
+      #     当发生时，首先解析 like status 的 show，发现是个 relation 而且 relationable
+      #     是个 status，其实就是那个图片 status，会直接跳转到 图片 status 的 show，再解析
+      #     发现 statusable 是个 Publication，于是就跳到图片的 status 了。
       # （趁现在还能记清赶快记下，之后又要分析 :p）
       #
       when Project, User, Status
