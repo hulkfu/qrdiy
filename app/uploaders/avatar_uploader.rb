79f0400f3ddf3c3c7fa4b96cf3a8bc4a051a4514
Fu Hao <fudhao@gmail.com>
2017-01-16 09:56:48 +0800

    bug fix: create project and its avatar.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -16,8 +16,7 @@ class AvatarUploader < BaseUploader
 
   # 一个用户只有一个头像
   def filename
-    name = Digest::MD5.hexdigest(File.dirname(store_dir))
-    "#{name}.#{file.nil? ? 'png' : file.extension.downcase}"
+    "img.#{file.nil? ? 'png' : file.extension.downcase}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
