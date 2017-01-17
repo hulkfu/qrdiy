41119b5650bb12262aa7253a6f9635b6fca47b68
Fu Hao <fudhao@gmail.com>
2017-01-17 15:22:57 +0800

    Comments cache.

---
+++ app/helpers/caches_helper.rb
@@ -0,0 +1,19 @@
+##
+# cache 相关的 helper，主要是 cache_key
+#
+module CachesHelper
+  def cache_key_for_users(users)
+    return "" unless users
+    count = users.count
+    max_updated_at = users.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "users/all-#{count}-#{max_updated_at}"
+  end
+
+  def cache_key_for_comments(comments)
+    return "" unless comments
+    count          = comments.count
+    max_updated_at = comments.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    users = User.where(id: comments.map(&:user_id).uniq)
+    "comments/all-#{count}-#{max_updated_at}-#{cache_key_for_users(users)}"
+  end
+end
