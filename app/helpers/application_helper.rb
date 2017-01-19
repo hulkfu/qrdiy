eedacc0504d2ffae95fac7165eafe95520871c11
Fu Hao <fudhao@gmail.com>
2017-01-19 19:04:45 +0800

    add avatar tag cache.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,29 +5,31 @@ module ApplicationHelper
   #   version：   头像的版本。目前有: big, normal, small, tiny
   #
   def avatar_tag(avatarable, version = :big, opts = {})
-    img_class = "avatar #{version}"
+    Rails.cache.fetch [:avatar, avatarable, version, opts] do
+      img_class = "avatar #{version}"
 
-    if avatarable.blank?
-      return image_tag("avatars/default.png", class: img_class)
-    end
-
-    img =
-      if avatarable.avatar?
-        image_url = avatarable.avatar.url(version)
-        image_url += "?t=#{avatarable.updated_at.to_i}" if opts[:timestamp]
-        image_tag(image_url, class: img_class)
-      else
-        image_tag("avatars/default.png", class: img_class)
+      if avatarable.blank?
+        return image_tag("avatars/default.png", class: img_class)
       end
 
-    options = {
-      title: avatarable.name
-    }
+      img =
+        if avatarable.avatar?
+          image_url = avatarable.avatar.url(version)
+          image_url += "?t=#{avatarable.updated_at.to_i}" if opts[:timestamp]
+          image_tag(image_url, class: img_class)
+        else
+          image_tag("avatars/default.png", class: img_class)
+        end
 
-    if opts[:link] != false
-      link_to(raw(img), avatarable, options)
-    else
-      raw img
+      options = {
+        title: avatarable.name
+      }
+
+      if opts[:link] != false
+        link_to(raw(img), avatarable, options)
+      else
+        raw img
+      end
     end
   end
 
