b62620aee1509022a4b46115ae371ce2321a72a7
Fu Hao <fudhao@gmail.com>
2017-01-23 16:11:22 +0800

    update

--- db/migrate/20170106110222_refactor_user_and_user_profilt.rb
+++ db/migrate/20170106110222_refactor_user_and_user_profilt.rb
@@ -7,18 +7,18 @@ class RefactorUserAndUserProfilt < ActiveRecord::Migration[5.0]
     add_index :users, :name, unique: true
     add_index :users, :domain, unique: true
 
-    User.find_each do |u|
-      %w(name domain).each do |k|
-        u.send("#{k}=", "#{k}_#{u.profile.send(k)}_#{rand.to_s[-4..-1]}") if u && u.try(:profile)
-      end
-      if file = u.try(:profile).try(:avatar).try(:file).try(:file)
-        File.open(file) do |f|
-          u.avatar = f || ""
-        end
-      end
-
-      u.save!
-    end
+    # User.find_each do |u|
+    #   %w(name domain).each do |k|
+    #     u.send("#{k}=", "#{k}_#{u.profile.send(k)}_#{rand.to_s[-4..-1]}") if u && u.try(:profile)
+    #   end
+    #   if file = u.try(:profile).try(:avatar).try(:file).try(:file)
+    #     File.open(file) do |f|
+    #       u.avatar = f || ""
+    #     end
+    #   end
+    #
+    #   u.save!
+    # end
 
     %w(name domain avatar).each do |k|
       remove_column :user_profiles, k, :string
