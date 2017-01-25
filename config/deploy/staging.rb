2b0a6c05af4f1437a02b966c5450a94e525d0e84
Fu Hao <fudhao@gmail.com>
2017-01-25 16:25:19 +0800

    Make staging also run production env.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,10 +3,11 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'qrdiy.com', user: 'deploy', roles: %w{app db web}
+server 'next.qrdiy.com', user: 'deploy', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+set :rails_env, :production
 
 
 # role-based syntax
